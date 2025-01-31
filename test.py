41b2705787e21d078ed6bfb9dd610d6aaff5b7e7
Mark <markus.98@list.ru>
2025-01-31 14:50:26 +0700

    cha cha cha

---
+++ test.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def test_add_todo(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+
+    context.close()
+    browser.close()
